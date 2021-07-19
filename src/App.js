d6a146001922dde0c9749366280b2b8a3b4ecff9
Gonzalez-EXT Enrique (uie19442) <13795+uie19442@users.noreply.github.geo.conti.de>
2021-07-19 01:03:35 -0500

    Add list of items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
+import {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
 import {API} from "aws-amplify";
 import {withAuthenticator, AmplifySignOut} from "@aws-amplify/ui-react";
 
+const initialFormState = {nombre:''}
+
 function App() {
+    const [articulos, setArticulos] = useState([]);
+    const [formData, setFormData] = useState(initialFormState);
+
+    useEffect(() => {fetchArticulos();}, [])
+
+    async function fetchArticulos(){
+        const apiData = await API.graphql({query: 'listArticulos'})
+        setArticulos(apiData.data.listArticulos.items);
+    }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Ya tenemos auth!</h1>
       </header>
+        <div style={{marginBottom: 30}}>
+            {
+                articulos.map(articulo =>(
+                    <div key={articulo.id}>
+                        <h2>articulo.nombre</h2>
+                    </div>
+                ))
+            }
+        </div>
         <AmplifySignOut />
     </div>
   );
