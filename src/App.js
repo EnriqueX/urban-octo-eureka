c670cf38755d3e42a25b08c7298ace00ca07bb0d
Gonzalez-EXT Enrique (uie19442) <13795+uie19442@users.noreply.github.geo.conti.de>
2021-07-21 01:56:47 -0500

    Primer maquetado del dashboard

--- src/App.js
+++ src/App.js
@@ -6,66 +6,34 @@ import {withAuthenticator, AmplifySignOut} from "@aws-amplify/ui-react";
 import {listArticulos} from "./graphql/queries";
 import {createArticulo, createCompra, createVenta, deleteArticulo, deleteCompra, deleteVenta} from "./graphql/mutations";
 
-const initialFormState = {nombre:'', descripcion: '', sustanciaActiva: ''}
-
-function App() {
-    const [articulos, setArticulos] = useState([]);
-    const [formData, setFormData] = useState(initialFormState);
-
-    useEffect(() => {fetchArticulos();}, [])
-
-    async function fetchArticulos(){
-        const apiData = await API.graphql({query: listArticulos})
-        setArticulos(apiData.data.listArticulos.items);
-    }
-
-    async function saveArticulo(){
-        if(!formData.nombre) return;
-        await API.graphql({query: createArticulo, variables: {input: formData}});
-        setArticulos([...articulos, formData]);
-        setFormData(initialFormState);
-    }
-
-    async function dropArticulo({id}){
-        const newArticulosArray = articulos.filter(articulo => articulo.id !== id);
-        setArticulos(newArticulosArray);
-        await API.graphql({query: deleteArticulo, variables: {input: {id}}});
-    }
-
-  return (
-    <div className="App">
-        <h1>Farmacia</h1>
-        <input
-            placeholder="nombre"
-            value={formData.nombre}
-            onChange={e => setFormData({...formData, 'nombre': e.target.value})}
-        />
-        <input
-            placeholder="descripcion"
-            value={formData.descripcion}
-            onChange={e => setFormData({...formData, 'descripcion': e.target.value})}
-        />
-        <input
-            placeholder="sustancia activa"
-            value={formData.sustanciaActiva}
-            onChange={e => setFormData({...formData, 'sustanciaActiva': e.target.value})}
-        />
-        <button onClick={saveArticulo}>Crear articulo</button>
-        <div style={{marginBottom: 30}}>
-            {
-                articulos.map(articulo =>(
-                    <div key={articulo.id || articulo.nombre}>
-                        <h2>{articulo.nombre}</h2>
-                        <p>{articulo.descripcion}</p>
-                        <p>{articulo.sustanciaActiva}</p>
-                        <button onClick={() => deleteArticulo(articulo.id)}>Borrar articulo</button>
-                    </div>
-                ))
-            }
-        </div>
-        <AmplifySignOut />
+const loading = (
+    <div className="pt-3 text-center">
+        <div className="sk-spinner sk-spinner-pulse"></div>
     </div>
-  );
+)
+
+// Containers
+const TheLayout = React.lazy(() => import('./containers/TheLayout'));
+
+const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
+const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
+
+class App extends Component {
+
+    render() {
+        return (
+            <HashRouter>
+                <React.Suspense fallback={loading}>
+                    <Switch>
+                        <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
+                        <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
+                        <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
+                    </Switch>
+                </React.Suspense>
+            </HashRouter>
+        );
+    }
 }
+
 
 export default withAuthenticator(App);
